4fe0d083b2bfd870833b51b001692f4075a07dab
Kirill Bespalov <kbespalov@mirantis.com>
2017-06-29 19:02:13 +0300

    [ keystone_policy.py ] Add yaml support
    What:
    
    Allow to read/write rules in json/yaml formats
    
    Why:
    
    1) Since Newton release oslo.policy supports both of the formats:
    yaml and json.
    
    2) oslopolicy-policy-generator produces an output only in yaml format
    since the release, so yaml format expected as default.
    
    3) Nova newton+ package has no any default policy.json file.
    It has empty policy.yaml file, so we need to have ability to
    manage the file from the module.
    
    Change-Id: I39239ba5eebee39483607785698806c040937b4b

--- _modules/keystone_policy.py
+++ _modules/keystone_policy.py
@@ -2,6 +2,8 @@ import io
 import json
 import logging
 
+import yaml
+
 LOG = logging.getLogger(__name__)
 
 
@@ -12,10 +14,10 @@ def __virtual__():
 def rule_list(path, **kwargs):
     try:
         with io.open(path, 'r') as file_handle:
-            rules = json.loads(file_handle.read())
-        rules =  {str(k): str(v) for (k, v) in rules.items()}
+            rules = yaml.safe_load(file_handle) or {}
+        rules = {str(k): str(v) for (k, v) in rules.items()}
     except Exception as e:
-        msg = "Unable to load policy JSON %s: %s" % (path, repr(e))
+        msg = "Unable to load policy file %s: %s" % (path, repr(e))
         LOG.debug(msg)
         rules = {'Error': msg}
     return rules
@@ -30,9 +32,13 @@ def rule_delete(name, path, **kwargs):
         del rules[name]
         try:
             with io.open(path, 'w') as file_handle:
-                file_handle.write(unicode(json.dumps(rules, indent=4)))
+                if path.endswith('json'):
+                    serialized = json.dumps(rules, indent=4)
+                else:
+                    serialized = yaml.safe_dump(rules, indent=4)
+                file_handle.write(unicode(serialized))
         except Exception as e:
-            msg = "Unable to save policy json: %s" % repr(e)
+            msg = "Unable to save policy file: %s" % repr(e)
             LOG.error(msg)
             return {'Error': msg}
         ret = 'Rule {0} deleted'.format(name)
@@ -47,9 +53,13 @@ def rule_set(name, rule, path, **kwargs):
         rules.update({name: rule})
         try:
             with io.open(path, 'w') as file_handle:
-                file_handle.write(unicode(json.dumps(rules, indent=4)))
+                if path.endswith('json'):
+                    serialized = json.dumps(rules, indent=4)
+                else:
+                    serialized = yaml.safe_dump(rules, indent=4)
+                file_handle.write(unicode(serialized))
         except Exception as e:
-            msg = "Unable to save policy JSON %s: %s" % (path, repr(e))
+            msg = "Unable to save policy file %s: %s" % (path, repr(e))
             LOG.error(msg)
             return {'Error': msg}
         return rule_get(name, path, **kwargs)
